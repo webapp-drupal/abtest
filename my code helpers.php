bc7b8e2640c70de970a060b9cdc6e5631230dc17
Vitalijus Charinas <Vitalijus.Charinas@avalon.net>
2019-12-23 15:23:04 +0000

    code clearup 1.3

--- my code helpers.php
+++ my code helpers.php
@@ -74,75 +74,75 @@
 
   //NODES URL 
 
-  // module_node_view($node, $view_mode, $langcode) {
-  //   if ($view_mode == 'full' && "Conditions") {
-  //     // Change $node->url to what works.
-  //     // Use the devel module and krumo() to check the contents of $node.
-  //     drupal_goto($node->url);
-  //   }
-  // }
+  module_node_view($node, $view_mode, $langcode) {
+    //if ($view_mode == 'full' && "Conditions") {
+      // Change $node->url to what works.
+      // Use the devel module and krumo() to check the contents of $node.
+      drupal_goto($node->url);
+    }
+  }
 
-  //$nodes = node_load_multiple ($nids);
+  $nodes = node_load_multiple ($nids);
     
-    //dpr($nids); // not displaing debug information
-    // $node_title ='';
-    // $headingA = '';
-    // $headingB = '';
-    // $Url = '';
+    dpr($nids); // not displaing debug information
+    $node_title ='';
+    $headingA = '';
+    $headingB = '';
+    $Url = '';
     
-    //return $nodes;
+    return $nodes;
 
-    //THIS IS NODE DISPLAY
+    THIS IS NODE DISPLAY
 
-    //  $output = array();//lets display them
-    //   if (!empty($result['node'])) {
-    //   $output['nodes'] = node_view_multiple(node_load_multiple(array_keys($result['node'])), 'teaser');
-    //   $output['pager']['#markup'] = theme('pager', $query->pager);
+     $output = array();//lets display them
+      if (!empty($result['node'])) {
+      $output['nodes'] = node_view_multiple(node_load_multiple(array_keys($result['node'])), 'teaser');
+      $output['pager']['#markup'] = theme('pager', $query->pager);
      
     
-    // }
+    }
   
-    // else {
-    //         $output['status']['#markup'] = t('No results were returned.');
-    //     }
+    else {
+            $output['status']['#markup'] = t('No results were returned.');
+        }
     
 
-    // dpr ($output); //not displaing debug information
-    // return $output;
+    dpr ($output); //not displaing debug information
+    return $output;
 
-     //   $nodes = entity_load('node', array_keys($result['node']));
+       $nodes = entity_load('node', array_keys($result['node']));
 
-  //   return node_view_multiple($nodes, 'teaser'); // how do i display or use nodes to edit and change the title for the nodes in query
+    return node_view_multiple($nodes, 'teaser'); // how do i display or use nodes to edit and change the title for the nodes in query
 
    
    
-  // }
+  }
   
  /**
   * Generates A/B test for the Node 
   */
 
  
-//  function abtest_headlines_node() {
+ function abtest_headlines_node() {
 
-//     $output = '';
-//     $output .= 'Test fired';
-//     // test variations in core WOW  SOLUTION 
-//     if (abtest('abtest_headlines')){
+    $output = '';
+    $output .= 'Test fired';
+    // test variations in core WOW  SOLUTION 
+    if (abtest('abtest_headlines')){
 
-//         //Variation Headline A
+        //Variation Headline A
 
-//     }
-//     else{
+    }
+    else{
 
-//         //Variation Headline B
+        //Variation Headline B
 
 
-//     }
+    }
 
-//     return $output;
+    return $output;
 
-//  }
+ }
 
 
 
@@ -150,119 +150,119 @@
    *  Getting nodes with required conditions
    */
 
-  //  function abtest_headlines_test () {
-  //   $node_id = '';
-  //   $node_title ='';
-  //   $headingA = '';
-  //   $headingB = '';
-  //   $Url = '';
-  //   $ga_tracking_path_A = "?utm_source=Home%20Page&utm_medium=website&utm_campaign=AB%20Test&utm_content=HeadlineA";
-  //   $ga_tracking_path_B = "?utm_source=Home%20Page&utm_medium=website&utm_campaign=AB%20Test&utm_content=HeadlineB";
-  //   //Query the nodes    
-  //   $query = new EntityFieldQuery;
-  //   $result = $query
-  //   ->entityCondition('entity_type', 'node')
-  //   ->entityCondition('bundle', 'blogs')
-  //   ->propertyCondition('status', 1)
-  //   ->fieldCondition('field_ab_test_boolean', 'value', 1, '=')
-  //   ->fieldCondition('field_headline_a', 'value', '', '!=')
-  //   ->fieldCondition('field_headline_b', 'value', '', '!=')
-  //   ->execute();
-  //   $nodes = array_keys($result['node']);
+   function abtest_headlines_test () {
+    $node_id = '';
+    $node_title ='';
+    $headingA = '';
+    $headingB = '';
+    $Url = '';
+    $ga_tracking_path_A = "?utm_source=Home%20Page&utm_medium=website&utm_campaign=AB%20Test&utm_content=HeadlineA";
+    $ga_tracking_path_B = "?utm_source=Home%20Page&utm_medium=website&utm_campaign=AB%20Test&utm_content=HeadlineB";
+    //Query the nodes    
+    $query = new EntityFieldQuery;
+    $result = $query
+    ->entityCondition('entity_type', 'node')
+    ->entityCondition('bundle', 'blogs')
+    ->propertyCondition('status', 1)
+    ->fieldCondition('field_ab_test_boolean', 'value', 1, '=')
+    ->fieldCondition('field_headline_a', 'value', '', '!=')
+    ->fieldCondition('field_headline_b', 'value', '', '!=')
+    ->execute();
+    $nodes = array_keys($result['node']);
 
-  //   foreach ($nodes as $node){
+    foreach ($nodes as $node){
 
-  //     $node = node_load($node);
+      $node = node_load($node);
       
-  //     //dpr($node);
-  //     global $base_url;
-  //     $node_title = "ORIGINAL TITLE: ".$node->title;
-  //     $node_id = $node->nid;
-  //     $headingA = "TEST TITLE VARIANT A: ".$node->field_headline_a[LANGUAGE_NONE] [0] ['value']; // string to be removed
-  //     $headingB = "TEST TITLE VARIANT B: ".$node->field_headline_b[LANGUAGE_NONE] [0] ['value']; // string to be removed
-  //     $Url = url(drupal_get_path_alias('node/' . $node->nid), array('absolute' => TRUE));
+      //dpr($node);
+      global $base_url;
+      $node_title = "ORIGINAL TITLE: ".$node->title;
+      $node_id = $node->nid;
+      $headingA = "TEST TITLE VARIANT A: ".$node->field_headline_a[LANGUAGE_NONE] [0] ['value']; // string to be removed
+      $headingB = "TEST TITLE VARIANT B: ".$node->field_headline_b[LANGUAGE_NONE] [0] ['value']; // string to be removed
+      $Url = url(drupal_get_path_alias('node/' . $node->nid), array('absolute' => TRUE));
       
      
-  //     dpr ($node_id);
-  //     dpr($node_title);
-  //     dpr($Url);
-  //     //dpr ($node);
+      dpr ($node_id);
+      dpr($node_title);
+      dpr($Url);
+      //dpr ($node);
       
-  //     //dpr($headingA);
-  //     //dpr($headingB);
+      //dpr($headingA);
+      //dpr($headingB);
       
-  //     //it is not real test
-  //     if ((int)$node_id < 7) {
-  //       //dpr ((int)$node_id);
+      //it is not real test
+      if ((int)$node_id < 7) {
+        //dpr ((int)$node_id);
 
-  //       $node_title = $headingA;
-  //       $Url = $Url.$ga_tracking_path_A;
-  //       dpr($node_title);
-  //       dpr($Url);
-  //     }
-  //     else {
-  //       //dpr ((int)$node_id);
-  //       $node_title = $headingB;
-  //       $Url = $Url.$ga_tracking_path_B;
-  //       dpr($node_title);
-  //       dpr($Url);
-  //     }
+        $node_title = $headingA;
+        $Url = $Url.$ga_tracking_path_A;
+        dpr($node_title);
+        dpr($Url);
+      }
+      else {
+        //dpr ((int)$node_id);
+        $node_title = $headingB;
+        $Url = $Url.$ga_tracking_path_B;
+        dpr($node_title);
+        dpr($Url);
+      }
 
       
 
-  //   }
+    }
 
     
 
-  // }
+  }
 
   /**
   *     Implements hook_menu() for the results
   */
 
-  // function abtest_headlines_menu() {
-  //     // Menus
-  //   $items = array();
-  //   $items['node/%node/abtest-headlines'] = array(
-  //       'title' => 'A/B Test Results',
-  //       'description' => 'Demonstrates A/B Test Results.',
-  //       'access callback' => TRUE,//'_abtest_headlines_menu_access',
-  //       'page callback' => 'abtest_headlines_results_node_tab',
-  //       //'page callback' => '_abtest_headlines_menu_access',
-  //       'type' =>  MENU_LOCAL_TASK,
-  //       'weight' => 10,
+  function abtest_headlines_menu() {
+      // Menus
+    $items = array();
+    $items['node/%node/abtest-headlines'] = array(
+        'title' => 'A/B Test Results',
+        'description' => 'Demonstrates A/B Test Results.',
+        'access callback' => TRUE,//'_abtest_headlines_menu_access',
+        'page callback' => 'abtest_headlines_results_node_tab',
+        //'page callback' => '_abtest_headlines_menu_access',
+        'type' =>  MENU_LOCAL_TASK,
+        'weight' => 10,
         
-  //     );
+      );
 
-  //   $items ['admin/reports/abtests-dashboard'] = array(
+    $items ['admin/reports/abtests-dashboard'] = array(
 
-  //       'title' => 'A/B Tests Report',
-  //       'description' => 'Admin pannel for multiple tests results',
-  //       'access callback' => TRUE,
-  //       'page callback' => 'abtest_headlines_results_node_tab',
-  //       //'page callback' => 'abtest_headlines_admin_dashboard',
-  //       'type' => MENU_NORMAL_ITEM,
+        'title' => 'A/B Tests Report',
+        'description' => 'Admin pannel for multiple tests results',
+        'access callback' => TRUE,
+        'page callback' => 'abtest_headlines_results_node_tab',
+        //'page callback' => 'abtest_headlines_admin_dashboard',
+        'type' => MENU_NORMAL_ITEM,
          
 
-  //   );
+    );
 
-  //     return $items;
+      return $items;
 
-  // }
+  }
   
 
-  // function _abtest_headlines_menu_access () {
+  function _abtest_headlines_menu_access () {
 
-  //   $node = menu_get_object();
+    $node = menu_get_object();
      
-  //   return $node->field_ab_test_boolean[0]['value'] === 1;
+    return $node->field_ab_test_boolean[0]['value'] === 1;
     
 
    
-  //   //  if ($node->field_ab_test_boolean[0]['value'] === 1) {
-  //   //   // Get the field value
-  //   //  return TRUE;
+    //  if ($node->field_ab_test_boolean[0]['value'] === 1) {
+    //   // Get the field value
+    //  return TRUE;
             
-  //   //}
+    //}
    
-  // }
+  }
---
+++ sites/all/modules/abtest_headlines/README.md
@@ -0,0 +1,31 @@
+# BEFORE TESTS #
+  
+     Create fields for the default Article content type machine name Blogs
+    1. field type bolean field_ab_test
+    2. field type text   field_headline_a
+    3. field type text   field_headline_a
+     probably needs to be implemented in .install file - this is posible upgrade
+ 
+# MODULE FUNCTIONALITY #
+ 
+    module depends on:
+    name = "A/B test (core)
+    description = An A/B testing API for creating tests quickly in code. 
+    name ="A/B Test Reporting dashboard"
+    description = A simple reporting dashboard for A/B tests.
+  
+    
+# IN MODULE #
+ 
+    WE do call all nodes in required conditions by EntityFieldQuery 
+    with checking all required conditions
+    before performing any tests and checking does it have to be tested and required information is entered 
+    it is filtred by query but to test fields are not empty will be extra care and helpfull ! Headline A and Headline B not empty !
+ 
+    1 geting nodes in a loop we are fetching title and url of the filtered Node
+    2 set the test id for each node required testing
+    3 run test and modify url to provide GA access to results
+    4 collect results 
+    5 display results for node in node tab no additional permissions required
+    6 display page with all tests results performed on admin accessable page (requires permission view results page)
+ 
